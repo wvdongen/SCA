9f24a78ac2216eaef77dfd8a294c4dccc84a238e
Andres Riancho <andres.riancho@gmail.com>
2012-08-13 18:28:22 -0300

    Latest (updated) version of samate tests. Still broken.

--- core/tests/test_samate.py
+++ core/tests/test_samate.py
@@ -25,7 +25,7 @@ from pymock import PyMockTestCase
 from core.sca_core import PhpSCA
 
 
-class TestSamate(PyMockTestCase):
+class TestSamate(object):
     '''
     Based on http://samate.nist.gov/SRD/view.php?tsID=31
     
@@ -38,26 +38,61 @@ class TestSamate(PyMockTestCase):
     SAMATE_TEST_DIR = os.path.join('core','tests','samate')
     SAMATE_MANIFEST = os.path.join('core','tests','samate','manifest.xml')
     
-    def setUp(self):
-        PyMockTestCase.setUp(self)
-    
     def _from_xml_get_test_cases(self):
         xp = XMLParser()
         parser = etree.XMLParser(target=xp)
         test_cases = etree.fromstring(file(self.SAMATE_MANIFEST).read(), parser)
         return test_cases
     
-    def test_samate(self):
+    def test_samate_generator(self):
         for test_case in self._from_xml_get_test_cases():
-            vuln_file = os.path.join(self.SAMATE_TEST_DIR, test_case.vuln_file)
-            analyzer = PhpSCA(infile=vuln_file)
-            print analyzer.get_vulns()
+            yield self.analyze_, test_case
+    
+    def analyze_(self, test_case):
+        for input_file_obj in test_case.files:
+            
+            input_file_name = os.path.join(self.SAMATE_TEST_DIR, input_file_obj.file)
+            analyzer = PhpSCA(infile=input_file_name)
+            
+            identified_vulns = []
+            
+            for vuln_type in analyzer.get_vulns():
+                for vuln_func_call, vuln_var in analyzer.get_vulns()[vuln_type]:
+                    identified_vulns.append((vuln_type, vuln_func_call._lineno))
+            
+            expected_vulns = []
+            for flaw in input_file_obj.flaws:
+                sca_name = SAMATE_TO_SCA[flaw.vuln_name]
+                expected_vulns.append((sca_name, flaw.vuln_line_no))
+            
+            #print set(expected_vulns), set(identified_vulns)
+            #assert set(expected_vulns) == set(identified_vulns)
+            print 'EXPECTED', set(expected_vulns)
+            print 'ID', set(identified_vulns)
 
 class XMLTestCase(object):
-    vuln_type = None
-    vuln_line_no = None
-    vuln_file = None
+    files = []
+    
+    def __init__(self, test_id):
+        self.test_id = test_id
+    
+    def __repr__(self):
+        return 'XMLTestCase for id %s' % self.test_id
 
+class TestFile(object):
+    flaws = []
+    
+    def __init__(self, file_name):
+        self.file = file_name
+    
+class Flaw(object):
+    def __init__(self, vuln_name, vuln_line_no):
+        self.vuln_name = vuln_name
+        self.vuln_line_no = vuln_line_no
+
+    def __str__(self):
+        return '%s at line number %s' % (self.vuln_name, self.vuln_line_no)
+    
 class XMLParser:
     tests = []
     def start(self, tag, attrib):
@@ -68,15 +103,27 @@ class XMLParser:
                 <flaw line="21" name="CWE-079: Failure to Sanitize Directives in a Web Page (Cross-site scripting XSS)"/>
             </file>
         </testcase>
-        '''        
+        '''
+        if tag == 'testcase':
+            self.current_test_case = XMLTestCase(attrib['id'])
+            self.tests.append(self.current_test_case)
+            
         if tag == 'file':
-            self.vuln_file = attrib['path']
+            self.current_file = TestFile(attrib['path'])
+            self.current_test_case.files.append(self.current_file)
+            
         elif tag == 'flaw':
-            xtc = XMLTestCase()
-            xtc.vuln_name = attrib['name']
-            xtc.vuln_line_no = attrib['line']
-            xtc.vuln_file = self.vuln_file
-            self.tests.append(xtc)
+            vuln_name = attrib['name']
+            vuln_line_no = attrib['line']
+            flaw = Flaw(vuln_name, vuln_line_no)
+            self.current_file.flaws.append(flaw)
     
     def close(self):
-        return self.tests  
+        return list(set(self.tests))
+
+SAMATE_TO_SCA = {
+                 'CWE-079: Failure to Sanitize Directives in a Web Page (Cross-site scripting XSS)':'XSS',
+                 'CWE-089: Failure to Sanitize Data within SQL Queries (SQL injection)':'SQL_INJECTION',
+                 'CWE-326: Weak Encryption':'WEAK_CRYPTO',
+                 'CWE-098: Insufficient Control of Filename for Include/Require Statement in PHP Program (PHP File Inclusion)':'FILE_INCLUDE',
+                 }
