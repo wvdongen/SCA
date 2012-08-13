ba8fa8612efb81244163e1a8eccabe49e3218770
Andres Riancho <andres.riancho@gmail.com>
2012-08-13 18:27:50 -0300

    Initial version of entry point.

---
+++ sca.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import sys
+import getopt
+
+from core.sca_core import PhpSCA
+
+usage_doc = '''sca - PHP static code analyzer
+
+Usage:
+
+    ./sca.py -h
+    ./sca.py -i <input_file_1.php],[input_file_n.php]>
+
+Options:
+
+    -h or --help
+        Display this help message.
+        
+    -i or --input-files=
+        Input files to analyze for vulnerabilities.
+
+For more info visit https://github.com/wvdongen/SCA
+'''
+
+def usage():
+    print usage_doc
+
+def main():
+    try:
+        long_options = ['help', 'input-files=']
+        opts, _ = getopt.getopt(sys.argv[1:], "hi:", long_options)
+    except getopt.GetoptError:
+        # print help information and exit:
+        usage()
+        return -3
+    
+    input_file_list = None
+    
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            return 0
+        if o in ('-i', '--input-files='):
+            input_file_list = a.split(',')
+    
+    if input_file_list is None:
+        usage()
+        return -3
+                
+    for input_file in input_file_list:
+        analyzer = PhpSCA(infile=input_file)
+        for vulnerability_type in analyzer.get_vulns():
+            print vulnerability_type
+            for vulnerability in analyzer.get_vulns()[vulnerability_type]:
+                print '    ', vulnerability
+
+if __name__ == "__main__":
+    err_code = main()
+    sys.exit(err_code)
+    
+
